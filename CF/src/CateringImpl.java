8427ded30a504329ce9adf9384bbd8ae1d23b849
lukas <lukas.dewachter@hotmail.com>
2022-11-29 18:10:57 +0100

    GUI caterer

--- CF/src/CateringImpl.java
+++ CF/src/CateringImpl.java
@@ -4,6 +4,11 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -14,6 +19,7 @@ import java.rmi.server.UnicastRemoteObject;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -26,31 +32,138 @@ public class CateringImpl extends UnicastRemoteObject implements Catering {
     byte[] secretKey;
     byte[] pseudonym;
     String CF;
+
+    JFrame frame = new JFrame("Caterer");
+    JTextField NameTextField = new JTextField();
+    JTextField BuisinessNumberTextField = new JTextField();
+    JTextField AddressTextField = new JTextField();
+    JTextField OutputTextField = new JTextField();
+    JButton logInButton = new JButton("log in");
+    JButton getSecretkey = new JButton("secret key");
+    JButton getPseudonym = new JButton("pseudonym");
+    JButton getQRCode = new JButton("QR code");
+    JLabel NameLabel = new JLabel("Name");
+    JLabel BuisinessNumberLabel = new JLabel("Buisiness Number");
+    JLabel AddressLabel = new JLabel("Address");
+    JLabel OutputLabel = new JLabel("output:");
+
     //hashing functie om hash in qrcode te genereren
     MessageDigest md = MessageDigest.getInstance("SHA-256");
 
     public CateringImpl() throws RemoteException, NoSuchAlgorithmException {
-
-        sc = new Scanner(System.in);
-        System.out.println("Enter unique name");
-        this.name = sc.nextLine();
-        System.out.println("Enter unique business number");
-        this.businessNumber = Integer.parseInt(sc.next());
-        System.out.println("Enter unique address");
-        this.address = sc.next();
-        System.out.println("Done!");
-        this.CF = businessNumber + name + address;
-
+        setFrame();
         try {
             // fire to localhost port 1099
             Registry myRegistry = LocateRegistry.getRegistry("localhost", 1099);
             registrar = (Registrar) myRegistry.lookup("Registrar");
 
-            String response = registrar.helloTo(name);
-            System.out.println(response);
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void setFrame(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(0,0,1000,400);
+        NameLabel.setBounds(0,20,10,10);
+        BuisinessNumberLabel.setBounds(0,0,10,10);
+        AddressLabel.setBounds(0,0,10,10);
+        NameTextField.setVisible(true);
+        BuisinessNumberTextField.setVisible(true);
+        AddressTextField.setVisible(true);
+        OutputLabel.setVisible(false);
+        OutputTextField.setVisible(false);
+        frame.getContentPane().add(NameLabel);
+        frame.getContentPane().add(BuisinessNumberLabel);
+        frame.getContentPane().add(AddressLabel);
+        frame.getContentPane().add(NameTextField);
+        frame.getContentPane().add(BuisinessNumberTextField);
+        frame.getContentPane().add(AddressTextField);
+        frame.getContentPane().add(OutputLabel);
+        frame.getContentPane().add(OutputTextField);
+        frame.getContentPane().add(logInButton);
+        frame.getContentPane().add(getQRCode);
+        frame.getContentPane().add(getPseudonym);
+        frame.getContentPane().add(getSecretkey);
+
+        frame.setLayout(new GridLayout(5,2));
+        frame.setSize(1000,400);
+        getPseudonym.setVisible(false);
+        getSecretkey.setVisible(false);
+        getQRCode.setVisible(false);
+        NameTextField.setEditable(true);
+        BuisinessNumberTextField.setEditable(true);
+        AddressTextField.setEditable(true);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.pack();
+
+        getQRCode.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    //TODO only generate QR each day
+                    genQRCode();
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+        getSecretkey.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    OutputTextField.setText(Arrays.toString(secretKey));
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+        getPseudonym.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    OutputTextField.setText(Arrays.toString(pseudonym));
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+        logInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    tryLogIn();
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void tryLogIn()throws  Exception{
+        if(!Objects.equals(NameTextField,"") && !Objects.equals(BuisinessNumberTextField,"") && !Objects.equals(AddressTextField,"")){
+            this.name = NameTextField.getText();
+            this.businessNumber = Integer.parseInt(BuisinessNumberTextField.getText());
+            this.address = AddressTextField.getText();
+            this.CF = businessNumber + name + address;
+            frame.remove(NameTextField);
+            frame.remove(BuisinessNumberTextField);
+            frame.remove(AddressTextField);
+            frame.remove(NameLabel);
+            frame.remove(BuisinessNumberLabel);
+            frame.remove(AddressLabel);
+            frame.remove(logInButton);
+            OutputLabel.setVisible(true);
+            OutputTextField.setVisible(true);
+            getQRCode.setVisible(true);
+            getSecretkey.setVisible(true);
+            getPseudonym.setVisible(true);
+            register();
+            String response = registrar.helloTo(name);
+            OutputTextField.setText(response);
         }
     }
 
@@ -60,15 +173,17 @@ public class CateringImpl extends UnicastRemoteObject implements Catering {
     }
 
     public static void main(String[] args) throws RemoteException, NoSuchAlgorithmException {
-        printMenu();
+
+        CateringImpl catering = new CateringImpl();
+        catering.register();
     }
 
     private static void printMenu() throws RemoteException, NoSuchAlgorithmException {
         Scanner s = new Scanner(System.in);
         int choice = 0;
         System.out.println("-----Enroll BarOwner-----");
-        CateringImpl catering = new CateringImpl();
-        catering.register();
+        //CateringImpl catering = new CateringImpl();
+        //catering.register();
 
         while (choice != -1) {
             System.out.println();
@@ -82,10 +197,10 @@ public class CateringImpl extends UnicastRemoteObject implements Catering {
                     choice = -1;
                     break;
                 case 2:
-                    System.out.println(Arrays.toString(catering.secretKey));
+                    //System.out.println(Arrays.toString(catering.secretKey));
                     break;
                 case 3:
-                    System.out.println(Arrays.toString(catering.pseudonym));
+                    //System.out.println(Arrays.toString(catering.pseudonym));
             }
         }
         s.close();
@@ -109,7 +224,7 @@ public class CateringImpl extends UnicastRemoteObject implements Catering {
                 matrix,
                 path.substring(path.lastIndexOf('.') + 1),
                 new File(path));
-        System.out.println("qr code output: " + data);
+        OutputTextField.setText(data);
     }
 
     @Override
